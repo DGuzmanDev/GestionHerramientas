955ec34b59eb686207402bbd73e8873f53ac262c
dx006328 <daniel.guzman@maxar.com>
2023-10-21 12:53:41 -0600

    Primer version con el template de VS y logica inicial para el endpoint de registro de colaboradores

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using GestionHerramientas.Models;
+using Microsoft.Data.SqlClient;
+using GestionHerramientas.Interfaces;
+using GestionHerramientas.Service;
+
+namespace GestionHerramientas.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    private readonly IServicioColaborador ServicioColaborador;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+        ServicioColaborador = new ServicioColaborador();
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpPost]
+    [Route("registrar")]
+    public Colaborador PostColaborador([FromBody] Colaborador colaborador)
+    {
+        try
+        {
+            if (colaborador != null)
+            {
+                return ServicioColaborador.Guardar(colaborador);
+            }
+            else {
+                throw new BadHttpRequestException("El cuerpo de la solicitud no es válido");
+            }
+        }
+        catch (Exception exception)
+        {
+            //TODO: Mejorar el mensaje, el manejo de errores top level y el tipo de Ex
+            Console.WriteLine("Error del controller: " + exception.Message);
+            throw exception;
+        }
+    }
+}
+
---
+++ Service/ServicioColaborador.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using GestionHerramientas.Datos;
+using GestionHerramientas.Interfaces;
+using GestionHerramientas.Models;
+using GestionHerramientas.Util;
+
+namespace GestionHerramientas.Service
+{
+    public class ServicioColaborador : IServicioColaborador
+    {
+        private ConectorDeDatos ConectorDeDatos;
+
+        public ServicioColaborador()
+        {
+            this.ConectorDeDatos = new ConectorDeDatos();
+        }
+
+        /**
+         * DOCS
+         */
+        public Colaborador Guardar(Colaborador colaborador)
+        {
+            try
+            {
+                if (ValidarIntegridadColaborador(colaborador))
+                {
+                    return ConectorDeDatos.GuardarColaborador(colaborador);
+                }
+                else
+                {
+                    //TDOD: Mejorar el mensaje y el tipo de EX
+                    throw new Exception("Colaborador invalido");
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error guardando nuevo Colaborador. Razon: " + error.Message);
+                throw;
+            }
+        }
+
+        /**
+         * DOCS
+         */
+        private bool ValidarIntegridadColaborador(Colaborador colaborador)
+        {
+            return colaborador == null || StringUtils.IsEmpty(colaborador.Identificacion)
+                     || StringUtils.IsEmpty(colaborador.Nombre)
+                     || StringUtils.IsEmpty(colaborador.Apellidos);
+        }
+    }
+}
+
